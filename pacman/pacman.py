5a9d8c8e815c4d1cd4d88f937039c3df2b31a19f
Hugo Albuquerque <hugoo_96@hotmail.com>
2022-05-03 21:07:42 -0300

    Primeira movimentacao

--- pacman/pacman.py
+++ pacman/pacman.py
@@ -1,15 +1,32 @@
 import pygame
 
 AMARELO = (255, 255, 0)
+PRETO = (0, 0, 0)
+VELOCIDADE = 0.1
+RAIO = 30
 pygame.init()
 
 tela = pygame.display.set_mode((640, 480), 0)
-
+x = 10
+y = 10
+vel_x = VELOCIDADE
+vel_y = VELOCIDADE
 while True:
     # Calcula as regras
+    x += vel_x
+    y += vel_y
+    if x + RAIO > 640:
+        vel_x = -VELOCIDADE
+    if x + -RAIO < 0:
+        vel_x = VELOCIDADE
+    if y + RAIO > 480:
+        vel_y = -VELOCIDADE
+    if y - RAIO < 0:
+        vel_y = VELOCIDADE
 
     # Pinta
-    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 5)
+    tela.fill(PRETO)
+    pygame.draw.circle(tela, AMARELO, (int(x), int(y)), RAIO, 0)
     pygame.display.update()
     # Eventos
     for e in pygame.event.get():
